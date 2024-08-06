09931196716b4f079e052a9b1d9042010506499e
Mr.Site <matinayoobi00@gmail.com>
2024-08-06 12:39:06 +0330

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
+// تاریخ
+
+function gregorian_to_jalali(gy, gm, gd) {
+  var g_d_m, jy, jm, jd, gy2, days;
+  g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+  gy2 = gm > 2 ? gy + 1 : gy;
+  days =
+    355666 +
+    365 * gy +
+    ~~((gy2 + 3) / 4) -
+    ~~((gy2 + 99) / 100) +
+    ~~((gy2 + 399) / 400) +
+    gd +
+    g_d_m[gm - 1];
+  jy = -1595 + 33 * ~~(days / 12053);
+  days %= 12053;
+  jy += 4 * ~~(days / 1461);
+  days %= 1461;
+  if (days > 365) {
+    jy += ~~((days - 1) / 365);
+    days = (days - 1) % 365;
+  }
+  if (days < 186) {
+    jm = 1 + ~~(days / 31);
+    jd = 1 + (days % 31);
+  } else {
+    jm = 7 + ~~((days - 186) / 30);
+    jd = 1 + ((days - 186) % 30);
+  }
+  return [jy, jm, jd];
+}
+
+MyDate = new Date();
+Year = MyDate.getFullYear();
+Month = MyDate.getMonth() + 1;
+Day = MyDate.getDate();
+
+var Result = document.getElementById("Result");
+
+Tarikh_Shamci = gregorian_to_jalali(Year, Month, Day);
+Result.innerHTML =
+  Tarikh_Shamci[0] + "/" + Tarikh_Shamci[1] + "/" + Tarikh_Shamci[2];
+
+// ساعت
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
 // اسلایدر
 
 let slideIndex = 1;
